f50d5b3758e746f5f8e56a6b2292a360afc63cef
Vishal naidu <vminorr@gmail.com>
2025-02-19 11:52:27 +0000

    Operations

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -8,9 +8,44 @@
 // "82abc => NaN"
 // true => 1, false => 0
 
-let isLoogedIn = "Vishal"
+// let isLoogedIn = "Vishal"
 
-let booleanIsLoogedIn = Boolean(isLoogedIn)
-console.log(booleanIsLoogedIn);
+// let booleanIsLoogedIn = Boolean(isLoogedIn)
+// console.log(booleanIsLoogedIn);
 
 // "" => false, "Vishal" => true
+
+
+// ****** Operations ******
+
+let value = "82"
+
+let negValue = -value  // Only work for numbers
+
+// console.log(negValue);
+// console.log(4**4);
+
+let str1 = "Jai Shree Ram"
+let str2 = " Bhaiyo aur unki behno"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log();
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+
+
+let gameCounter = 25
+gameCounter++;
+
+console.log(gameCounter);
