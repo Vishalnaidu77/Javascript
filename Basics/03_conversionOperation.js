23df0c9a1d2bf09fbec0a8fdc212fd2292daed6c
Vishal naidu <vminorr@gmail.com>
2025-02-19 11:27:09 +0000

    Conversion Operation

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// let run = true
+
+// let valueInNumber = Number(run)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "82" => 82
+// "82abc => NaN"
+// true => 1, false => 0
+
+let isLoogedIn = "Vishal"
+
+let booleanIsLoogedIn = Boolean(isLoogedIn)
+console.log(booleanIsLoogedIn);
+
+// "" => false, "Vishal" => true
