7a44de31dd178aea2e86b57907afedbdb73e4e4a
Vishal naidu <vminorr@gmail.com>
2025-02-20 06:55:40 +0000

    Datatypes summary

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -17,5 +17,46 @@ let isLoggedIn = false
 // Object => 
 
 
-console.log(typeof undefined); // Undefined
-console.log(typeof null);      // Object
+// console.log(typeof undefined); // Undefined
+// console.log(typeof null);      // Object
+
+
+// Summary 
+
+// Primitive DataTypes
+// 7 types:- String, Number, Boolean, null, undefine, Symbol, BigInt
+
+const run = 97;
+const strikeRate = 196.4;
+const isInjured = false;
+const dotBall = null;
+let battingOrder;
+
+const tShirtno = Symbol(18)
+const capNo = Symbol(18)
+
+// console.log(tShirtno === capNo);
+
+
+const totalRunTournaments = 5451112154858n // BigInt
+
+// Reference (Non primitive)
+// Arrays, Objects, Funtions 
+
+
+const players = ["Virat", "Joy", "Kane"]
+
+const myFnction = function(){
+    console.log("Hello");
+    
+}
+
+const myObject = {
+    name: "Vishal",
+    age: 22
+}
+
+console.log(typeof capNo);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
