519a3f41d116b56aec23898fd1cf274b4a255c12
Vishal naidu <vminorr@gmail.com>
2025-02-20 08:42:29 +0000

    Strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,25 @@
+let name = "Vishal"
+let role = "Pacer All-rounder"
+
+// Backticks, String interpolation
+
+console.log(`Hello my name is ${name}. I'm a ${role}.`);
+
+console.table([name, role])
+
+let name2 = new String('   Joe root   ')
+
+console.log(typeof name)
+console.log(name2[4]);
+console.log(name2.__proto__);
+console.log(name2.toUpperCase());
+console.log(name2.indexOf("r"));
+console.log(name2.repeat(4));
+console.log(name2.replace("root", "markaram"));
+console.log(name2.replaceAll("Joe root", "Kane williamson"));
+console.log(name2.slice(-6, 1));
+console.log(name2.substring(4,6));
+console.log(name2.trim());
+console.log(name2.split(" "))
+
+// console.log(name2);
